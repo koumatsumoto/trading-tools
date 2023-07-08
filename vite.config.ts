fae6e56d01e0126097e71d5ec37e2100be3f3395
kou <develop@kou-matsumoto.jp>
2023-07-08 22:08:05 +0900

    build: change module entry file and module name

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@
 import typescript from "@rollup/plugin-typescript";
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
+import packageJson from "./package.json";
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "TradingTools",
-      fileName: "trading-tools",
+      name: packageJson.name,
+      fileName: "index",
     },
   },
   plugins: [typescript()],
